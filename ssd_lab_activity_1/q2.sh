bf76653434290836738782a31a72d897473f8d17
Yash_Sampat <yashsampat23154@gmail.com>
2022-08-18 11:30:32 +0530

    Update q2.sh

--- ssd_lab_activity_1/q2.sh
+++ ssd_lab_activity_1/q2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls -lh $1 | grep "...x...-.."
+ls -la $1 | grep --colour -E "...x...-.." | grep --colour -Eo "\s.?[a-zA-Z0-9_]+.?[a-zA-Z]+$" | grep --colour -Eo ".?[a-zA-Z0-9_]+.?[a-zA-Z]+$" 
